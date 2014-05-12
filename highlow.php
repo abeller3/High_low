80ca929dce1fe1ff84a386626d8a4ee71dc085de
abeller3 <beller.amanda@yahoo.com>
2014-05-12 13:42:28 -0500

    edited format even further

--- highlow.php
+++ highlow.php
@@ -1,27 +1,34 @@
 <?php
+// create a number 
 $number = rand(1,100);
-$guess = 0;
+$guess = 1;
 // Write the output
 // Notice the space after?
+// prompt user guess a number
 fwrite(STDOUT, "Guess a number?\n");
 
-//Get the input from user
+//Get a guess from user
 $user_guess= fgets(STDIN);
 
 
-//evaluate
+//check the number and the guess 
+//if the guess is higher than number then tell the user "lower"
 while ($user_guess != $number) 
+// if the guess is lower than the number tell the user "higher"
 { 
 	if ($user_guess < $number) 
-{
+	}
 	fwrite(STDOUT, "Higher!\n");
-}
+	}	
 	elseif($user_guess > $number) 
-{
+	{
 	fwrite(STDOUT, "Lower! Try again\n");
-}
+	}
+// if guess and the nuber are the same say "winner!" and end the game
+// ad to the guess count 
 	$user_guess = fgets(STDIN);
 	$guess++;
+// end the loop
 }
 
 echo " Good guess! You got it in {$guess} tries\n";
